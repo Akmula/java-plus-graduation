cab391b39000b691be8e72be6363548a1907203b
Akmula <akmula@yandex.ru>
2025-07-21 01:33:06 +0300

    Этап 1. Подготовить проект к запуску в облачных средах
    Шаг 1. Изменить структуру проекта
    Шаг 2. Настроить Spring Cloud Eureka
    Шаг 3. Настроить Spring Cloud Config Server
    Шаг 4. Настроить Spring Cloud Gateway

--- stats/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/StatClient.java
@@ -1,22 +1,30 @@
 package ru.practicum.ewm.client;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.ewm.dto.EndpointHitDto;
 import ru.practicum.ewm.dto.ViewStatsDto;
-import org.springframework.web.client.RestClient;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 public class StatClient {
-    private final RestClient restClient;
+    private final DiscoveryClient discoveryClient;
+    private final String statsServerId;
 
-    public StatClient(String baseUrl) {
-        this.restClient = RestClient.builder().baseUrl(baseUrl).build();
+    public StatClient(DiscoveryClient discoveryClient, String statsServerId) {
+        this.discoveryClient = discoveryClient;
+        this.statsServerId = statsServerId;
     }
 
     public void sendHit(EndpointHitDto hitDto) {
-        restClient.post()
+        restClient().post()
                 .uri("/hit")
                 .body(hitDto)
                 .retrieve()
@@ -39,11 +47,51 @@ public class StatClient {
 
         String uri = uriBuilder.build().toUriString();
 
-        List<ViewStatsDto> response = restClient.get()
+        List<ViewStatsDto> response = restClient().get()
                 .uri(uri)
                 .retrieve()
-                .body(new ParameterizedTypeReference<>() {});
+                .body(new ParameterizedTypeReference<>() {
+                });
 
         return response != null ? response : List.of();
+    }
+
+    private RetryTemplate getRetryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(3000L);
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
+        retryPolicy.setMaxAttempts(3);
+        retryTemplate.setRetryPolicy(retryPolicy);
+
+        return retryTemplate;
+    }
+
+    private ServiceInstance getInstance() {
+        try {
+            return discoveryClient
+                    .getInstances(statsServerId)
+                    .getFirst();
+        } catch (Exception exception) {
+            throw new RuntimeException(
+                    "Ошибка обнаружения адреса сервиса статистики с id: " + statsServerId,
+                    exception
+            );
+        }
+    }
+
+    private RestClient restClient() {
+
+        URI baseUri = makeUri();
+
+        return RestClient.builder().baseUrl(baseUri.toString()).build();
+    }
+
+    private URI makeUri() {
+        ServiceInstance instance = getRetryTemplate().execute(cxt -> getInstance());
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort());
     }
 }
